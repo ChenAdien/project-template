4b6d0225acfac1e4cdd6f08cdb38dc86e262e5da
Chen_Z <38248854+ChenAdien@users.noreply.github.com>
2022-11-23 00:31:14 +0800

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "standard-with-typescript",
+        "prettier",
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json"],
+    },
+    plugins: ["vue"],
+    rules: {},
+}
